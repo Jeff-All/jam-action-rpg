d9aa2ef4885cf55cde7af95a01d27164c852106c
Jeff-All <41645485+Jeff-All@users.noreply.github.com>
2025-11-24 10:25:02 -0600

    Adding classes and races

---
+++ scripts/resources/class.gd
@@ -0,0 +1,24 @@
+class_name Class
+
+extends Resource
+
+@export var name: String
+
+@export var threat: int
+
+@export var attack: int
+@export var defense: int
+
+@export var strength: int
+@export var agility: int
+@export var magic: int
+
+@export var armor: int
+@export var durability: int
+@export var health: int
+@export var stamina: int
+@export var mana: int
+
+@export var health_recovery: int
+@export var stamina_recovery: int
+@export var mana_recovery: int
